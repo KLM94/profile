fe01aaafbc29a4732bf66a6a966bf064bb9c763b
Kirsty McGlynn <kirsty.lmcg@gmail.com>
2020-10-28 12:19:25 +0000

    Store and pull friends list and login data from JSON file and render on screen

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,17 +1,35 @@
+import React, { Component } from "react";
 import ProfilePicture from "../Images/profilepic.png";
 import Form from "../Components/Form";
+import { data } from "../data/data.json";
 
-export default function Profile() {
-  return (
-    <main>
-      <h1 className="header">My Profile</h1>
+const { friendsList, loginHistory } = data;
 
-      <img className="profilePicture" src={ProfilePicture} alt="Profile" />
+class Profile extends Component {
+  /* Changed to Class component because will be using state  -- consider extracting into seperate component*/
 
-      <Form />
+  render() {
+    return (
+      <main>
+        <h1 className="header">My Profile</h1>
 
-      {/* Removed into seperate component as the file got larger and untidy */}
-      {/* Look into JSON file and extract information from there */}
-    </main>
-  );
+        <img className="profilePicture" src={ProfilePicture} alt="Profile" />
+        {/* Profile picture maybe needs adding to JSON file and extracted from there. */}
+
+        <Form />
+
+        <p>Friends List:</p>
+        <ul>
+          {friendsList.map((fl) => (
+            <li>{fl}</li>
+          ))}
+        </ul>
+
+        {/* Removed Form into seperate component as file got larger and untidy. */}
+        {/* Look into JSON file and extract information from there. */}
+      </main>
+    );
+  }
 }
+
+export default Profile;
